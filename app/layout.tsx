549eb36f4671e31015245f42a531143ed2da32c3
Alisultan Omariv <alisultanomariv@Alisultans-MacBook-Air.local>
2025-12-24 21:50:38 +0500

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           <ConvexClientProvider>
             <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem={false}
             disableTransitionOnChange
             >
